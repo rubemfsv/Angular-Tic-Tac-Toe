58256d94246307cd58f1be17245ff15dd6fb62f4
Rubem <rfsv@ic.ufal.br>
2021-05-04 09:30:58 -0300

    feature: create tic tac toe service

--- src/app/tic-tac-toe/tic-tac-toe.module.ts
+++ src/app/tic-tac-toe/tic-tac-toe.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TicTacToeComponent } from './';
+import { TicTacToeService } from './shared';
 
 @NgModule({
-  declarations: [
-    TicTacToeComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports:[
-    TicTacToeComponent
-  ]
+  imports: [CommonModule],
+  declarations: [TicTacToeComponent],
+  exports: [TicTacToeComponent],
+  providers: [TicTacToeService],
 })
-export class TicTacToeModule { }
+export class TicTacToeModule {}
